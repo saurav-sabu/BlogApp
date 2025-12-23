31488591efcd633d953b8b7bd1b36cf76748b7aa
saurav-sabu <saurav.sabu9@gmail.com>
2025-12-24 00:47:06 +0530

    added api for create,get and get all

---
+++ main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI,Depends,status, Response, HTTPException
+from schemas import Blog
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+import models
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/blog",status_code=status.HTTP_201_CREATED)
+def create_blog(request:Blog,db:Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title,body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+@app.get("/blog")
+def get_all_blogs(db:Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get("/blog/{id}",status_code=200)
+def get_blog_by_id(id:int,db:Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog {id} not found")
+    return blog
