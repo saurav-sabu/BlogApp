6cc2282f5ea1cd78a31d01341206ea931959e008
saurav-sabu <saurav.sabu9@gmail.com>
2025-12-24 23:25:51 +0530

    added delete and update api code

--- main.py
+++ main.py
@@ -24,6 +24,26 @@ def create_blog(request:Blog,db:Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+@app.put("/blog/{id}",status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id:int,request:Blog,db:Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with {id} not found")
+    blog.update(request.dict())
+    db.commit()
+    return {"status":"done"}
+
+
+@app.delete("/blog/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(id:int,db:Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with {id} not found")
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return {"status":"done"}
+
+
 @app.get("/blog")
 def get_all_blogs(db:Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
